b53e4522efc1ebe11fd3889b681febfec137c2fd
Ferran <ferranhendriks@hotmail.com>
2020-09-24 15:16:09 +0200

    Input project files

---
+++ Dimitri/Engines/Input/src/input.cpp
@@ -0,0 +1,8 @@
+/*This source code copyrighted by Ferran Hendriks aka WORKHORSE' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+
+
