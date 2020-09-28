ab1f6aba83c42ba09ff266fbe07f3cf29d44e452
khcbaude <38493543+khcbaude@users.noreply.github.com>
2020-09-28 16:02:10 +0200

    Window create and delete

---
+++ Dimitri/Engines/Graphics/src/Handlers/WindowHandler.cpp
@@ -0,0 +1,75 @@
+#include "WindowHandler.h"
+
+bool Input::WindowHandler::init(const char* title, int xpos, int ypos, int height, int width)
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,  width, height, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+
+    return success;
+}
+
+void Input::WindowHandler::close()
+{
+    //Deallocate surface
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+
+void Input::WindowHandler::CreateWindow(const char* title, int xpos, int ypos, int height, int width)
+{
+    //Start up SDL and create window
+    if (!init(title, xpos, ypos, height, width))
+    {
+        //foutmelding ofzo?
+        printf("Failed to initialize!\n");
+    }
+    else {
+        //succes
+        //printf("Ik kom hier!\n");
+    }
+}
+
+void Input::WindowHandler::DeleteWindow()
+{
+    close();
+}
+
+int main(int argc, char** argv) {
+
+    Input::WindowHandler handler = Input::WindowHandler{};
+    handler.CreateWindow("Dimitri", 0, 0, 720, 1000);
+    //Wait two seconds
+    SDL_Delay(10000);
+
+    handler.DeleteWindow();
+    return 0;
+}
--- Dimitri/Engines/Input/src/input.cpp
+++ Dimitri/Engines/Input/src/input.cpp
@@ -6,10 +6,10 @@ and may not be redistributed without written permission.*/
 #include <stdio.h>
 #include <iostream>
 
-int main(int argc, char** argv) {
-	std::cout << "Have " << argc << " arguments:" << std::endl;
-	for (int i = 0; i < argc; ++i) {
-		std::cout << argv[i] << std::endl;
-	}
-	return 0;
-}
+//int main(int argc, char** argv) {
+//	std::cout << "Have " << argc << " arguments:" << std::endl;
+//	for (int i = 0; i < argc; ++i) {
+//		std::cout << argv[i] << std::endl;
+//	}
+//	return 0;
+//}
