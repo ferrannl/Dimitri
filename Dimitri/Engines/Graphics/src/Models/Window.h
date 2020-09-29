595c4ffbcc588cf210ca80ab0cb8a619a7bb7324
Max Peters <max.t.peters@outlook.com>
2020-09-29 14:39:12 +0200

    window model created and made a few other changes

--- Dimitri/Engines/Graphics/src/Models/Window.h
+++ Dimitri/Engines/Graphics/src/Models/Window.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <string> 
+namespace Models {
+	class Window {
+	private:
+		int _x;
+		int _y;
+		int _height;
+		int _width;
+		const char* _title;
+	public:
+		Window(const int x, const int y, const int height, const int width, const char* title) 
+			: _x{ x }, _y{ y }, _height{ height }, _width{ width }, _title{ title } {}
+
+		const int getX();
+		const int getY();
+		const int getHeight();
+		const int getWidth();
+		const char* getTitle();
+	};
+}
