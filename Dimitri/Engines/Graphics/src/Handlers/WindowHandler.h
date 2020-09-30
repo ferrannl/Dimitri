4bfa8bf9fac1367d45f3a764079aa8cff0b47566
Max Peters <max.t.peters@outlook.com>
2020-09-30 16:33:50 +0200

    removed unnessecary code

--- Dimitri/Engines/Graphics/src/Handlers/WindowHandler.h
+++ Dimitri/Engines/Graphics/src/Handlers/WindowHandler.h
@@ -13,28 +13,15 @@ namespace Handlers
 		Models::Window* _window;
 
 		public: 
+			WindowHandler() {
+				_window = nullptr;
+			}
+
 			void create_window(const char* title, int xpos, int ypos, int height, int width);
 			void add_sprites(std::vector<Models::Sprite*> sprites);
 			std::vector<Models::Sprite*> get_sprites();
 
 			void update_window();
 			Models::Window* get_window();
-
-
-	private:
-		//Starts up SDL and creates window
-		bool init(const char* title, int xpos, int ypos, int height, int width);
-
-		//Frees media and shuts down SDL
-		void close();
-
-		//The window we'll be rendering to
-		SDL_Window* gWindow = NULL;
-
-		//The surface contained by the window
-		SDL_Surface* gScreenSurface = NULL;
-
-		//The image we will load and show on the screen
-		SDL_Surface* gHelloWorld = NULL;
 	};
 }
