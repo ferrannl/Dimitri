fe5ea0816d0ef5211701e68212282d768bd508a4
khcbaude <38493543+khcbaude@users.noreply.github.com>
2020-10-05 11:42:18 +0200

    Play music kan een file afspelen

--- Dimitri/Game/SandBox/Main.cpp
+++ Dimitri/Game/SandBox/Main.cpp
@@ -1,9 +1,16 @@
 #include "PhysicsCollisionDemo.h"
+#include "../../Engines/Audio/src/Handlers/MusicPlayer.h"
 
 int main(int argc, char** argv) 
 {
+    Handlers::MusicPlayer music = {};
+    music.init();
+    music.play_music();
+
     PhysicsCollisionDemo demo = {};
     demo.start_demo();
+
+    music.close();
 
     return 0;
 }
