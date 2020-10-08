b5af013c3dd7a8e79cb75a65dbedfb080487ee40
Billyyy013 <bhfnooij@avans.nl>
2020-10-08 12:03:44 +0200

    keyenum

--- Dimitri/Engines/Input/src/adapters/KeyCodeAdapter.cpp
+++ Dimitri/Engines/Input/src/adapters/KeyCodeAdapter.cpp
@@ -15,14 +15,14 @@ KeyEnum KeyCodeAdapter::translateToEnum(SDL_Event event)
 	switch (event.key.keysym.sym)
 	{
 	case SDLK_LEFT:
-		return KEY_PRESS_LEFT;
+		return KeyEnum::KEY_PRESS_LEFT;
 	case SDLK_RIGHT:
-		return KEY_PRESS_RIGHT;
+		return KeyEnum::KEY_PRESS_RIGHT;
 	case SDLK_UP:
-		return KEY_PRESS_UP;
+		return KeyEnum::KEY_PRESS_UP;
 	case SDLK_DOWN:
-		return KEY_PRESS_DOWN;
+		return KeyEnum::KEY_PRESS_DOWN;
 	case SDLK_SPACE:
-		return KEY_PRESS_JUMP;
+		return KeyEnum::KEY_PRESS_JUMP;
 	}
 }
