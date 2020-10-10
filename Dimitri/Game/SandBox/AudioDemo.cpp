19054c049985533c9925e3fe996ef3f98ca391bb
Xia de Visser <yxvisser@avans.nl>
2020-10-10 10:23:52 +0200

    create const function  and add c++ sleep function instead of sdl in audiodemo

--- Dimitri/Game/SandBox/AudioDemo.cpp
+++ Dimitri/Game/SandBox/AudioDemo.cpp
@@ -5,28 +5,28 @@ AudioDemo::AudioDemo()
 	_audio_controller = std::make_unique<Controllers::AudioController>();
 }
 
-void AudioDemo::play_music()
+void AudioDemo::play_music() const
 {
 	const std::string name = std::string("music");
     _audio_controller->add_music(name, (SDL_GetBasePath() + std::string{ "assets/audio/beat.wav" }).c_str());
     _audio_controller->play_audio(name);
-    SDL_Delay(1000);
+	sleep_for(1000ms);
     _audio_controller->pause_audio(name);
-    SDL_Delay(1000);
+	sleep_for(1000ms);
     _audio_controller->resume_audio(name);
-    SDL_Delay(1000);
+	sleep_for(1000ms);
     _audio_controller->stop_audio(name);
 }
 
-void AudioDemo::play_sound()
+void AudioDemo::play_sound() const
 {
 	const std::string name = std::string("sound");
 	_audio_controller->add_sound(name, (SDL_GetBasePath() + std::string{ "assets/audio/beat.wav" }).c_str());
 	_audio_controller->play_audio(name);
-	SDL_Delay(250);
+	sleep_for(1000ms);
 	_audio_controller->pause_audio(name);
-	SDL_Delay(250);
+	sleep_for(1000ms);
 	_audio_controller->resume_audio(name);
-	SDL_Delay(250);
+	sleep_for(1000ms);
 	_audio_controller->stop_audio(name);
 }
--- Dimitri/Game/SandBox/AudioDemo.h
+++ Dimitri/Game/SandBox/AudioDemo.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "../../Engines/Audio/src/Controllers/AudioController.h"
+#include <chrono>
+#include <thread>
+using namespace std::this_thread;
+using namespace std::chrono_literals;
 
 class AudioDemo {
 private:
 	std::unique_ptr<Controllers::AudioController> _audio_controller;
 public:
 	AudioDemo();
-	void play_music();
-	void play_sound();
+	void play_music() const;
+	void play_sound() const;
 };
