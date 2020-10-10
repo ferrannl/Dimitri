39c5b27c0a17be098226981af957ef7d8eeb592d
khcbaude <38493543+khcbaude@users.noreply.github.com>
2020-10-10 17:30:28 +0200

    Update Main.cpp

--- Dimitri/Game/SandBox/Main.cpp
+++ Dimitri/Game/SandBox/Main.cpp
@@ -3,14 +3,14 @@
 
 int main(int argc, char** argv) 
 {
-    AudioDemo audio_demo = {};
+   /* AudioDemo audio_demo = {};
   
-    std::thread audio_thread(&AudioDemo::play_music, audio_demo);
+    std::thread audio_thread(&AudioDemo::play_music, audio_demo);*/
 
     std::shared_ptr<PhysicsCollisionDemo> demo = std::make_shared<PhysicsCollisionDemo>();
     demo->subscribe_to_input(demo);
     demo->start_demo();
     
-    audio_thread.join();
+    //audio_thread.join();
     return 0;
 }
