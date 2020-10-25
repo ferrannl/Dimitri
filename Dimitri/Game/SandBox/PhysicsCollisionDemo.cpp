22d0fbc67b0f72afae8bbd4e24a78b14a913fde8
xiadevisser <38435110+xiadevisser@users.noreply.github.com>
2020-10-25 14:21:44 +0100

    fix sandbox project: create shared and unique pointers (#25)
    * fix sandbox project: create shared and unique pointers
    
    * add sprites to demo
    
    * delete commented code in shapefacade

--- Dimitri/Game/SandBox/PhysicsCollisionDemo.cpp
+++ Dimitri/Game/SandBox/PhysicsCollisionDemo.cpp
@@ -2,11 +2,11 @@
 
 PhysicsCollisionDemo::PhysicsCollisionDemo()
 {
-	PhysicsCollisionDemo::graphicsController = Graphics::Controllers::GraphicsController{};
-	worldController = Controllers::WorldController{};
-	_inputController = std::make_shared<Input::Controllers::InputController>();
+	PhysicsCollisionDemo::graphics_controller = Graphics::Controllers::GraphicsController{};
+	world_controller = Controllers::WorldController{};
+	input_controller = std::make_shared<Input::Controllers::InputController>();
 	sprites = std::make_shared<std::vector<std::unique_ptr<Graphics::Models::Sprite>>>();
-	shapes = std::vector<Models::Shape>{};
+	shapes = {};
 }
 
 void PhysicsCollisionDemo::start_demo()
@@ -14,32 +14,44 @@ void PhysicsCollisionDemo::start_demo()
 	if (create_window(1080, 720) == NULL) {
 		return;
 	}
-	
+
 	std::string image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/img.png" };
-	create_sprite(350, 600, 1, 50, 50, image.c_str(), 0, Graphics::Enums::FlipEnum::VERTICAL);
-	create_sprite(300, 200, 1, 300, 50, image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL);
+	std::string bg_image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/bg.png" };
+	std::string ground_end_image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/ground_end.png" };
+	std::string ground_image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/ground.png" };
+	std::string player_image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/player.png" };
+
+	create_sprite(350, 300, 1, 50, 100, player_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE);
+	create_sprite(300, 200, 1, 50, 50, ground_end_image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL);
+	create_sprite(350, 200, 1, 50, 50, ground_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE);
+	create_sprite(400, 200, 1, 50, 50, ground_end_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE);
+	// create walls around screen
 	create_sprite(0, 720, 1, 1080, 1, image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL);
 	create_sprite(0, -1, 1, 1080, 1, image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL);
 	create_sprite(-1, 0, 1, 1, 720, image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL);
 	create_sprite(1080, 0, 1, 1, 720, image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL);
-	graphicsController.add_sprites(sprites);
-	create_shape(350, 600, 50, 50, true);
-	create_shape(300, 200, 300, 50, false);
+	create_sprite(0, 0, 0, 1080, 720, bg_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE);
+	graphics_controller.add_sprites(sprites);
+	create_shape(350, 300, 50, 100, true);
+	create_shape(300, 200, 50, 50, false);
+	create_shape(350, 200, 50, 50, false);
+	create_shape(400, 200, 50, 50, false);
+	// create walls around screen
 	create_shape(0, 720, 1080, 1, false); // top    
 	create_shape(0, -1, 1080, 1, false); // bottom    
 	create_shape(-1, 0, 1, 720, false); // left
 	create_shape(1080, 0, 1, 720, false); // right
-	shapes = worldController.get_shapes();
+	shapes = world_controller.get_shapes();
 
 	std::thread demo_thread(&PhysicsCollisionDemo::run, this);
-	_inputController->poll_events();
+	input_controller->poll_events();
 	demo_thread.join();
 }
 
 int PhysicsCollisionDemo::create_window(int width, int height)
 {
-	if (graphicsController.create_window("Test", height, width) != NULL) {
-		worldController.setup_world(width, height);
+	if (graphics_controller.create_window("Test", height, width) != NULL) {
+		world_controller.setup_world(width, height);
 	}
 	else {
 		return NULL;
@@ -54,38 +66,40 @@ void PhysicsCollisionDemo::create_sprite(int x, int y, int z, int width, int hei
 void PhysicsCollisionDemo::create_shape(int x, int y, int width, int height, bool is_dynamic)
 {
 	std::vector<std::pair<float, float>> positions{ {0.0f,0.0f}, {0.0f,height},{width, height}, {width, 0.0f} };
-	worldController.create_shape("polygon", x, y, positions, is_dynamic);
+	world_controller.create_shape("polygon", x, y, positions, is_dynamic);
 }
 
 void PhysicsCollisionDemo::run()
 {
 	while (true)
 	{
-		for (int i = 0; i < shapes->size(); i++)
-		{
-			sprites->at(i)->set_x(static_cast<int>(shapes->at(i)->get_x()));
-			sprites->at(i)->set_y(static_cast<int>(shapes->at(i)->get_y()));
-			sprites->at(i)->set_angle(static_cast<int>(shapes->at(i)->get_angle()));
+		for (int i = 0; i < shapes.get()->size(); i++)
+		{		
+			sprites->at(i)->set_x(static_cast<int>(shapes.get()->at(i)->get_x()));
+			sprites->at(i)->set_y(static_cast<int>(shapes.get()->at(i)->get_y()));
+			sprites->at(i)->set_angle(static_cast<int>(shapes.get()->at(i)->get_angle()));
 		}
-		graphicsController.update_window();
-		worldController.simulate();
+		graphics_controller.update_window();
+		world_controller.simulate();
 
 		sleep_for(5ms);
 	}
-	worldController.destroy_bodies();
-	graphicsController.get_window()->destroy();
+	world_controller.destroy_bodies();
+	graphics_controller.get_window()->destroy();
 }
 
 void PhysicsCollisionDemo::update(const Input::Enums::EventEnum& event)
 {
 	switch (event) {
 	case Input::Enums::EventEnum::KEY_PRESS_LEFT:
-		shapes[0].move_x(-1);
-		sprites->at(0)->set_x(shapes[0].get_x());
+		shapes.get()->at(0)->move_x(-1);
+		sprites->at(0)->set_x(shapes.get()->at(0)->get_x());
+		sprites->at(0)->set_flip_status(Graphics::Enums::FlipEnum::HORIZONTAL);
 		break;
 	case Input::Enums::EventEnum::KEY_PRESS_RIGHT:
-		shapes[0].move_x(1);
-		sprites->at(0)->set_x(shapes[0].get_x());
+		shapes.get()->at(0)->move_x(1);
+		sprites->at(0)->set_x(shapes.get()->at(0)->get_x());
+		sprites->at(0)->set_flip_status(Graphics::Enums::FlipEnum::NONE);
 		break;
 	default: 
 		std::cout << "geen reactie";
@@ -94,7 +108,7 @@ void PhysicsCollisionDemo::update(const Input::Enums::EventEnum& event)
 
 void PhysicsCollisionDemo::subscribe_to_input(std::shared_ptr<PhysicsCollisionDemo> demo)
 {
-	_inputController->subscribe(demo);
+	input_controller->subscribe(demo);
 }
 
 
