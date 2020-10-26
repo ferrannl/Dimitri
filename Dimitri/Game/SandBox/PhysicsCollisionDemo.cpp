f9ae0b04e1108b23fbf04124b14c8c0f05f22735
Olav Gerritsen <olavjens@hotmail.com>
2020-10-26 11:57:09 +0100

    Removed commented code

--- Dimitri/Game/SandBox/PhysicsCollisionDemo.cpp
+++ Dimitri/Game/SandBox/PhysicsCollisionDemo.cpp
@@ -21,16 +21,11 @@ void PhysicsCollisionDemo::start_demo()
 	std::string ground_image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/wall.png" };
 	std::string player_image = Utility::Helpers::get_base_path() + std::string{ "/assets/images/player.png" };
 	create_sprite(350, 300, 1, 75, 75, player_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE, true);
-	//create_sprite(300, 200, 1, 50, 50, ground_end_image.c_str(), 0, Graphics::Enums::FlipEnum::HORIZONTAL, false);
 	create_sprite(300, 200, 1, 300, 50, ground_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE, false);
-	//create_sprite(400, 200, 1, 50, 50, ground_end_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE, false);
 	create_sprite(0, 0, 0, 1080, 720, bg_image.c_str(), 0, Graphics::Enums::FlipEnum::NONE, true);
 
 	graphicsController.add_sprites(sprites);
-	//create_shape(350, 100, 100, 100, true);
 	create_shape(350, 300, 75, 75, true);
-	/*create_shape(300, 200, 50, 50, false);
-	create_shape(350, 200, 50, 50, false);*/
 	create_shape(300, 175, 300, 50, false);	
 	create_shape(0, 645, 1080, 100, false); // top
 	create_shape(0, -175, 1080, 100, false); // bottom
