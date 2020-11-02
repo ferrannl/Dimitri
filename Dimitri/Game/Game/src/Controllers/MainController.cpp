3ff77eeb1a7ce3dde4ec0c1d05e74c803e65db72
Olav Gerritsen <38222368+olavgerritsen98@users.noreply.github.com>
2020-11-02 15:17:28 +0100

    Feature/music (#28)
    * start
    
    * Finished
    
    * Update
    
    * Update
    
    * Final updates
    
    * Merged

--- Dimitri/Game/Game/src/Controllers/MainController.cpp
+++ Dimitri/Game/Game/src/Controllers/MainController.cpp
@@ -4,5 +4,8 @@ Controllers::MainController::MainController()
 {
 	_window_controller = std::make_unique<WindowController>();
 	_input_controller = std::make_shared<Controllers::InputController>();
+	_level = std::make_shared<Models::Level>();
+	_level->add_music("level1", "/assets/audio/billy.wav");
+	_level->play_music("level1");
 	_input_controller->poll_events();
 }
--- Dimitri/Game/Game/src/Controllers/MainController.h
+++ Dimitri/Game/Game/src/Controllers/MainController.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "InputController.h"
 #include "WindowController.h"
+#include "../Models/Level.h"
 #include <memory>
 namespace Game {
 	namespace Controllers {
 		class MainController {
 		private:
+			std::shared_ptr<Models::Level> _level;
 			std::shared_ptr<Controllers::InputController> _input_controller;
 			std::unique_ptr<Controllers::WindowController> _window_controller;
 		public:
