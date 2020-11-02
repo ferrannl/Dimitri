404ff640150c7eace96d6e426b36c80aa33cdac5
Olav Gerritsen <olavjens@hotmail.com>
2020-11-02 13:00:37 +0100

    Final updates

--- Dimitri/Game/Game/Controllers/MainController.cpp
+++ Dimitri/Game/Game/Controllers/MainController.cpp
@@ -5,7 +5,7 @@ Controllers::MainController::MainController()
 	_window_controller = std::make_unique<WindowController>();
 	_input_controller = std::make_shared<Controllers::InputController>();
 	_level = std::make_shared<Models::Level>();
-	_level->add_music_for_level("level1", "/assets/audio/billy.wav");
-	_level->play_music_for_level("level1");
+	_level->add_music("level1", "/assets/audio/billy.wav");
+	_level->play_music("level1");
 	_input_controller->poll_events();
 }
--- Dimitri/Game/Game/Models/Level.h
+++ Dimitri/Game/Game/Models/Level.h
@@ -13,12 +13,12 @@ namespace Game
 			std::shared_ptr<Game::Controllers::AudioController> _audio_controller;
 		public:
 			Level();
-			void add_music_for_level(std::string audio_name, std::string path);
-			void add_sound_for_level(std::string audio_name, std::string path);
-			void play_music_for_level(std::string audio_name);
-			void stop_music_for_level(std::string audio_name);
-			void pause_music_for_level(std::string audio_name);
-			void resume_music_for_level(std::string audio_name);
+			void add_music(std::string audio_name, std::string path);
+			void add_sound(std::string audio_name, std::string path);
+			void play_music(std::string audio_name);
+			void stop_music(std::string audio_name);
+			void pause_music(std::string audio_name);
+			void resume_music(std::string audio_name);
 
 		};
 	}
