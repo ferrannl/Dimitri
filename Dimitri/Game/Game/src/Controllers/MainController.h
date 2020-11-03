512857d586308dc9a78417804f65718c645bd6e3
khcbaude <38493543+khcbaude@users.noreply.github.com>
2020-11-03 20:17:36 +0100

    Nog wat extra comments

--- Dimitri/Game/Game/src/Controllers/MainController.h
+++ Dimitri/Game/Game/src/Controllers/MainController.h
@@ -8,13 +8,28 @@ namespace Game {
 	namespace Controllers {
 		class MainController {
 		private:
+			/**
+			*	Level model
+			*/
 			std::shared_ptr<Models::Level> _level;
+			/**
+			*	Input Controller
+			*/
 			std::shared_ptr<Controllers::InputController> _input_controller;
+			/**
+			*	Window Controller
+			*/
 			std::shared_ptr<Controllers::WindowController> _window_controller;
+			/**
+			*	Level Controller
+			*/
 			std::shared_ptr<Controllers::LevelController> _level_controller;
 		public:
 			MainController();
 
+			/**
+			*	Starts polling input controller
+			*/
 			void run();
 		};
 	}
--- Dimitri/Game/Game/src/Models/IInteractable.h
+++ Dimitri/Game/Game/src/Models/IInteractable.h
@@ -5,6 +5,10 @@ namespace Game {
 		class IInteractable : public Game::Models::IObject{
 		public:
 			IInteractable(int x, int y, int z, int height, int width);
+			
+			/**
+			*	Virtual method to for example change object state
+			*/
 			virtual void interact() = 0;
 		};
 	}
