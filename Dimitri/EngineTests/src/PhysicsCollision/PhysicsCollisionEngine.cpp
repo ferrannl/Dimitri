4deb8395d8332d386c21946b5d09f449d976bc39
Olav Gerritsen <olavjens@hotmail.com>
2020-11-03 17:30:46 +0100

    Created tests

--- Dimitri/EngineTests/src/PhysicsCollision/PhysicsCollisionEngine.cpp
+++ Dimitri/EngineTests/src/PhysicsCollision/PhysicsCollisionEngine.cpp
@@ -1,15 +1,81 @@
 #include "CppUnitTest.h"
-#include <src\Controllers\GraphicsController.h>
-
+#include <src/Controllers/WorldController.h>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace EngineTests
 {
 	TEST_CLASS(PhysicsCollisionEngine)
 	{
-	public:
-		TEST_METHOD(METHOD_NAME)
+	private:
+		std::shared_ptr<PhysicsCollision::Models::Shape> create_shape()
 		{
+			PhysicsCollision::Controllers::WorldController ctr = PhysicsCollision::Controllers::WorldController{};
+			std::shared_ptr<PhysicsCollision::Models::Shape> shape = ctr.create_shape(PhysicsCollision::Enums::ShapeEnum::Square, 8.0f, 10.0f, 20.0f, 20.0f, true);
+
+			return shape;
+		}
+
+		PhysicsCollision::Controllers::WorldController create_world_setup()
+		{
+			PhysicsCollision::Controllers::WorldController ctr = PhysicsCollision::Controllers::WorldController{};
+
+			ctr.setup_world(1080, 720);
+			ctr.create_shape(PhysicsCollision::Enums::ShapeEnum::Square, 8.0f, 10.0f, 20.0f, 20.0f, true);
+
+
+			return ctr;
+		}
+	public:
+		TEST_METHOD(Create_Shape)
+		{
+			std::shared_ptr<PhysicsCollision::Models::Shape> shape = create_shape();
+
+			Assert::IsNotNull(shape.get());
+			Assert::AreEqual(true, shape->get_is_dynamic());
+		}
+
+		TEST_METHOD(Gravity)
+		{
+			PhysicsCollision::Controllers::WorldController ctr = create_world_setup();
+
+			std::vector<std::shared_ptr<PhysicsCollision::Models::Shape>> shapes = ctr.get_shapes();
+
+			ctr.simulate();
+			
+			Assert::AreNotEqual(10.0f, shapes[0]->get_y());
+		}
+
+		TEST_METHOD(Move_Left)
+		{
+			PhysicsCollision::Controllers::WorldController ctr = create_world_setup();
+
+			std::vector<std::shared_ptr<PhysicsCollision::Models::Shape>> shapes = ctr.get_shapes();
+			shapes[0]->get_shape_facade()->move_x(-1);
+			ctr.simulate();
+
+			Assert::AreNotEqual(8.0f, shapes[0]->get_x());
+		}
+
+		TEST_METHOD(Move_Right)
+		{
+			PhysicsCollision::Controllers::WorldController ctr = create_world_setup();
+
+			std::vector<std::shared_ptr<PhysicsCollision::Models::Shape>> shapes = ctr.get_shapes();
+			shapes[0]->get_shape_facade()->move_x(1);
+			ctr.simulate();
+
+			Assert::AreNotEqual(8.0f, shapes[0]->get_x());
+		}
+
+		TEST_METHOD(Move_Up)
+		{
+			PhysicsCollision::Controllers::WorldController ctr = create_world_setup();
+
+			std::vector<std::shared_ptr<PhysicsCollision::Models::Shape>> shapes = ctr.get_shapes();
+			shapes[0]->get_shape_facade()->move_y();
+			ctr.simulate();
+
+			Assert::AreNotEqual(10.0f, shapes[0]->get_y());
 		}
 	};
 }
