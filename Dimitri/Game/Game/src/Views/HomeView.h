6b3a24410ceadf7068ad365c163719e83e685983
Ferran <ferranhendriks@hotmail.com>
2020-11-12 18:15:55 +0100

    Home

--- Dimitri/Game/Game/src/Views/HomeView.h
+++ Dimitri/Game/Game/src/Views/HomeView.h
@@ -1,1 +1,36 @@
 #pragma once
+#include <..\Game\Game\src\Views\View.h>
+#include <src\Helpers\BasePathHelper.h>
+
+/**
+*	Namespace for the game
+*/
+namespace Game {
+	/**
+	*	Namespace for the views
+	*/
+	namespace Views {
+		/**
+		*	Containing all the code to draw the HomeView using the graphics controller
+		*/
+		class HomeView : public View {
+		public:
+			HomeView(const std::shared_ptr<Graphics::Controllers::GraphicsController>& graphics_controller);
+
+			/**
+			*	Initializes textures
+			*/
+			void init_textures();
+
+			/**
+			* Add the textures to the _graphics_controller
+			*/
+			void draw();
+
+			/**
+			*	Returns if view is visible
+			*/
+			bool is_visible() const override;
+		};
+	}
+}
