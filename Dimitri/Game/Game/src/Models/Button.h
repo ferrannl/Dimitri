601f38d35ac7023cf557d69765ab183c1fb39945
Ferran <ferranhendriks@hotmail.com>
2020-11-12 20:20:17 +0100

    WIP
    https://www.youtube.com/watch?v=DKx6DxGY5ZA

---
+++ Dimitri/Game/Game/src/Models/Button.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../Controllers/AudioController.h"
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <src\Models\Texture.h>
+#include "../Models/IObject.h"
+#include "../Controllers/PhysicsCollisionController.h"
+
+/**
+*	Namespace for the game
+*/
+namespace Game {
+	/**
+	*	Namespace for the Models
+	*/
+	namespace Models {
+		/**
+		*	Contains all code necessary for the button object
+		*/
+		class Button
+		{
+		private:
+
+		public:
+			Button();
+		};
+	}
+
+}
--- Dimitri/Game/Game/src/Views/HelpView.cpp
+++ Dimitri/Game/Game/src/Views/HelpView.cpp
@@ -43,6 +43,23 @@ namespace Game {
 		}
 	}
 
+	void EventManager::update() {
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			switch (e.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				//do whatever you want to do after a mouse button was pressed,
+				// e.g.:
+				mousePress(e.button);
+				break;
+
+			}
+		}
+	}
+
 	void Views::HelpView::draw()
 	{
 		for (auto& t : _textures) {
