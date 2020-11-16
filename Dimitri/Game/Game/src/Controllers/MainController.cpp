73e1f079bf6251d7eb98c868c333ed4a99bb8e66
khcbaude <38493543+khcbaude@users.noreply.github.com>
2020-11-16 16:47:44 +0100

    Update MainController.cpp

--- Dimitri/Game/Game/src/Controllers/MainController.cpp
+++ Dimitri/Game/Game/src/Controllers/MainController.cpp
@@ -25,7 +25,7 @@ void Controllers::MainController::update(const Events::InputEvent& object)
 			_window_controller->open_view("credits");
 			_window_controller->open_view("fps");
 			_level_controller->stop();
-			_window_controller->set_level_size(1280, 720);
+			_window_controller->set_level_size(_window_controller->get_window_width(), _window_controller->get_window_height());
 			_input_controller->unsubscribe(_level_controller);
 		}
 		break;
