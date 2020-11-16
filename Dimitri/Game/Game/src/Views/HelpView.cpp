8476abf30702a4d111b18365d1b95505d1db9e48
Billyyy013 <bhfnooij@avans.nl>
2020-11-16 15:40:49 +0100

    oneliners

--- Dimitri/Game/Game/src/Views/HelpView.cpp
+++ Dimitri/Game/Game/src/Views/HelpView.cpp
@@ -13,28 +13,23 @@ namespace Game {
 		std::vector <std::string> gameplay = { "Dimitri moet", "ongezien infiltreren", "in de geheime", "russische basis" };
 		std::string path = Utility::Helpers::get_base_path() + std::string{ "/assets/fonts/font1.ttf" };
 		Color color = { 255, 255, 255 };
-		std::shared_ptr<Graphics::Models::Text> texture = std::make_shared<Graphics::Models::Text>("Helpscherm", color, (window_width / 2) - 120, 600, 10, 70, 240, 0, path, true);
-		std::shared_ptr<Graphics::Models::Text> controls_header = std::make_shared<Graphics::Models::Text>("Knoppen", color, window_width / 4 - 75, 500, 10, 60, 150, 0, path, true);
-		std::shared_ptr<Graphics::Models::Text> play_header = std::make_shared<Graphics::Models::Text>("Doel", color, (window_width / 4) * 3 - 45, 500, 10, 60, 90, 0, path, true);
 
-		_textures.push_back(texture);
-		_textures.push_back(controls_header);
-		_textures.push_back(play_header);
+		_textures.push_back(std::make_shared<Graphics::Models::Text>("Helpscherm", color, (window_width / 2) - 120, 600, 10, 70, 240, 0, path, true));
+		_textures.push_back(std::make_shared<Graphics::Models::Text>("Knoppen", color, window_width / 4 - 75, 500, 10, 60, 150, 0, path, true));
+		_textures.push_back(std::make_shared<Graphics::Models::Text>("Doel", color, (window_width / 4) * 3 - 45, 500, 10, 60, 90, 0, path, true));
 
 		int start_y = 425;
 		int step = 50;
 		for (int i = 0; i < controls.size(); i++) {
 			int width = controls[i].length() * 15;
 			int x = window_width / 4 - width / 2;
-			texture = std::make_shared<Graphics::Models::Text>(controls[i], color, x, start_y - step * i, 10, 50, width, 0, path, true);
-			_textures.push_back(texture);
+			_textures.push_back(std::make_shared<Graphics::Models::Text>(controls[i], color, x, start_y - step * i, 10, 50, width, 0, path, true));
 		}
 
 		for (int i = 0; i < gameplay.size(); i++) {
 			int width = gameplay[i].length() * 15;
 			int x = (window_width / 4) * 3 - width / 2;
-			texture = std::make_shared<Graphics::Models::Text>(gameplay[i], color, x, start_y - step * i, 10, 50, width, 0, path, true);
-			_textures.push_back(texture);
+			_textures.push_back(std::make_shared<Graphics::Models::Text>(gameplay[i], color, x, start_y - step * i, 10, 50, width, 0, path, true));
 		}
 	}
 
