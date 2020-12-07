9900afeae278153e9b04faf92c95f3f62f939a53
Max Peters <max.t.peters@outlook.com>
2020-12-07 22:15:30 +0100

    erros but sad

--- Dimitri/DocumentHandler/src/Readers/JsonReader.cpp
+++ Dimitri/DocumentHandler/src/Readers/JsonReader.cpp
@@ -1,9 +1,12 @@
 #include "JsonReader.h"
-#include <json/json.h>
+#include <single_include/nlohmann/json.hpp>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <iostream>
+
+// for convenience
+
 
 std::vector<std::vector<int>> DocumentHandler::Readers::JsonReader::Read(const std::filesystem::path& path)
 {
@@ -20,26 +23,27 @@ std::vector<std::vector<int>> DocumentHandler::Readers::JsonReader::Read(const s
 		raw_json.append(line);
 		std::cout << line.c_str() << std::endl;
 	}
+	
+	//nlohmann::json::json::parse();
+	//Json::CharReaderBuilder builder;
+	//const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+	//const auto rawJsonLength = static_cast<int>(raw_json.length());
+	//JSONCPP_STRING err;
+	//Json::Value root;
 
-	Json::CharReaderBuilder builder;
-	const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
-	const auto rawJsonLength = static_cast<int>(raw_json.length());
-	JSONCPP_STRING err;
-	Json::Value root;
+	//if (!reader->parse(raw_json.c_str(), raw_json.c_str() + rawJsonLength, &root,
+	//	&err)) {
+	//	std::cout << "error" << std::endl;
+	//	throw std::runtime_error("Cannot parse json file");
+	//}
 
-	if (!reader->parse(raw_json.c_str(), raw_json.c_str() + rawJsonLength, &root,
-		&err)) {
-		std::cout << "error" << std::endl;
-		throw std::runtime_error("Cannot parse json file");
-	}
-
-	try {
-		Json::Value a = root.get("layers", 0) ;
-		std::cout << a;
-	}
-	catch (std::exception e) {
-		std::cout << e.what();
-	}
+	//try {
+	//	Json::Value a = root.get("layers", 0) ;
+	//	std::cout << a;
+	//}
+	//catch (std::exception e) {
+	//	std::cout << e.what();
+	//}
 
 
 
