c944f089ef31b8e02487254b3e2633b841da86dd
Olav Gerritsen <olavjens@hotmail.com>
2020-12-08 10:29:38 +0100

    Update

--- Dimitri/Engines/PhysicsCollision/src/Models/Square.cpp
+++ Dimitri/Engines/PhysicsCollision/src/Models/Square.cpp
@@ -1,6 +1,6 @@
 #include "Square.h"
 using namespace PhysicsCollision;
 
-Models::Square::Square(int x, int y, int height, int width, bool is_dynamic,bool is_interactable, Enums::ShapeEnum type) : Shape(x, y, height, width, is_dynamic, is_interactable, type)
+Models::Square::Square(float x, float y, float height, float width, bool is_dynamic,bool is_interactable, Enums::ShapeEnum type) : Shape(x, y, height, width, is_dynamic, is_interactable, type)
 {
 }
