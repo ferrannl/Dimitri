d5a138ecd70f125f06a9305240660d6d4804f162
Olav Gerritsen <olavjens@hotmail.com>
2020-12-08 12:58:43 +0100

    Update

--- Dimitri/Game/Game/src/Commands/OpenHomeViewCommand.cpp
+++ Dimitri/Game/Game/src/Commands/OpenHomeViewCommand.cpp
@@ -13,12 +13,14 @@ namespace Game {
 			auto h_ctrl = _main_controller->get_home_controller();
 			if (!w_ctrl->is_active("home")) {
 				w_ctrl->clear_views();
+				
 				w_ctrl->open_view("home");
 				w_ctrl->open_view("fps");
-				for (auto b : _main_controller->get_home_controller()->get_advertisement_buttons())
+				for (auto b : h_ctrl->get_advertisement_buttons())
 				{
 					b->initialize_textures();
 				}
+				w_ctrl->add_textures(h_ctrl->get_textures(), "home");
 				l_ctrl->stop();
 				a_ctrl->play_audio("homescreen1");
 				w_ctrl->set_scene_size(w_ctrl->get_window_height(), w_ctrl->get_window_width());
