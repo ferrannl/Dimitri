8ffbb5825ef867e7f5614e3eb3b798c705f9684c
Billyyy013 <bhfnooij@avans.nl>
2020-12-09 12:50:04 +0100

    update

--- Dimitri/Game/Game/src/Commands/OpenHomeViewCommand.cpp
+++ Dimitri/Game/Game/src/Commands/OpenHomeViewCommand.cpp
@@ -11,7 +11,7 @@ namespace Game {
 			auto i_ctrl = _main_controller->get_input_controller();
 			auto a_ctrl = _main_controller->get_audio_controller();
 			auto h_ctrl = _main_controller->get_home_controller();
-			if (!w_ctrl->is_active("home")) {
+			if (!w_ctrl->is_active("home") && !w_ctrl->is_active("pause_level")) {
 				w_ctrl->toggle_view_visibility("timer");
 				w_ctrl->clear_views();
 				w_ctrl->open_view("home");
