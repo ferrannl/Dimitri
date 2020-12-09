35ea0332fd4eb3c272f045ec72aed45f198bd52b
Billyyy013 <bhfnooij@avans.nl>
2020-12-09 12:56:44 +0100

    record above 10 works

--- Dimitri/Game/Game/src/Views/HighscoreView.cpp
+++ Dimitri/Game/Game/src/Views/HighscoreView.cpp
@@ -28,9 +28,7 @@ namespace Game {
 			place_margin = place_margin + 48;
 		}
 		place_margin = 0;
-		if (_record_times.size() > 10) {
-			_record_times.pop_back();
-		}
+		
 		std::sort(_record_times.begin(), _record_times.end());
 		for (int i = 0; i < _record_times.size(); i++) {
 			_textures.push_back(std::make_shared<Graphics::Models::Text>(_record_times[i].substr(0,6), color, window_width / 4 + 300, (window_height - 225) - place_margin, 1, 40, 100, 0, path, true, Graphics::Models::Center{ 0, 0 }, false));
@@ -45,6 +43,9 @@ namespace Game {
 	}
 	void Views::HighscoreView::add_record(std::string record)
 	{
+		if (_record_times.size() >= 10) {
+			_record_times.pop_back();
+		}
 		_record_times.push_back(record);
 		_textures.clear();
 		init_textures();
