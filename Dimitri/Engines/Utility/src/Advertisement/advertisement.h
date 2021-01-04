4c269e2b8c8137faca1767cabf8b0fdf84ea3a78
Olav Gerritsen <olavjens@hotmail.com>
2021-01-04 12:25:06 +0100

    doxygen

--- Dimitri/Engines/Utility/src/Advertisement/advertisement.h
+++ Dimitri/Engines/Utility/src/Advertisement/advertisement.h
@@ -20,14 +20,35 @@
 #define UTILITY_API
 #endif
 
+/**
+* \namespace Utility
+* \brief Namespace for the utility project
+*/
 namespace Utility{
+	/**
+	* \namespace Advertisement
+	* \brief Namespace for the advertisement in the utility project
+	*/
 	namespace Advertisement {
+		/**
+		* \class Advertisement
+		* \brief Class contains the the methods to retreive the advertisement images
+		*/
 		class UTILITY_API advertisement {
 		private:
 		public:
 			 advertisement();
+			 /**
+			 * /brief Deletes all the advertisement images before adding them
+			 */
 			 void delete_images(const std::string& destination);
+			 /**
+			 * /brief Downloads the images from DropBox
+			 */
 			 void http_download_images(const std::string& destination, const std::string& zip_location);
+			 /**
+			 * /brief Unzips the files retreived from DropBox
+			 */
 			 void unzip_file(const std::string& destination, const std::string& zip_location);
 		};
 	}
